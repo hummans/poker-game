8509afc06e46239b4b8e384c8cfbf9e4605e90cb
Simon <simonfong6@gmail.com>
2018-10-07 17:27:09 -0700

    Implemented community strings for each state

--- poker-game/poker.py
+++ poker-game/poker.py
@@ -83,16 +83,86 @@ class Hand(object):
             "Hand:\n"
             "Card 1: {hand.card_1}\n"
             "Card 2: {hand.card_2}\n"
-            ).format(hand=self)
+        ).format(hand=self)
         return hand_str
 
 
 class Community(object):
 
+    MAX_TURN = 3
+
+    # States
+    PREFLOP = 0
+    FLOP = 1
+    TURN = 2
+    RIVER = 3
+
+    # Names of each state.
+    STATE_NAMES = [
+        "Pre-Flop",
+        "Flop",
+        "Turn",
+        "River"
+    ]
+
     def __init__(self, cards):
+        self.state = 0
         self.flop = cards[:3]
         self.turn = cards[:4]
         self.river = cards[:]
+
+    def next(self):
+        self.state += 1
+        self.state = min(self.state, Community.MAX_TURN)
+
+    def __str__(self):
+        if self.state is Community.PREFLOP:
+            state_name = Community.STATE_NAMES[self.state]
+            community_str = (
+                "Community Cards:\n"
+                "State: {state}\n"
+                "HIDDEN"
+            ).format(state=state_name)
+        elif self.state is Community.FLOP:
+            state_name = Community.STATE_NAMES[self.state]
+            community_str = (
+                "Community Cards:\n"
+                "State: {state}\n"
+                "{}\n"
+                "{}\n"
+                "{}"
+            ).format(
+                state=state_name,
+                *self.flop)
+        elif self.state is Community.TURN:
+            state_name = Community.STATE_NAMES[self.state]
+            community_str = (
+                "Community Cards:\n"
+                "State: {state}\n"
+                "{}\n"
+                "{}\n"
+                "{}\n"
+                "{}"
+            ).format(
+                state=state_name,
+                *self.turn)
+        elif self.state is Community.RIVER:
+            state_name = Community.STATE_NAMES[self.state]
+            community_str = (
+                "Community Cards:\n"
+                "State: {state}\n"
+                "{}\n"
+                "{}\n"
+                "{}\n"
+                "{}\n"
+                "{}"
+            ).format(
+                state=state_name,
+                *self.river)
+        else:
+            community_str = "ERROR"
+
+        return community_str
 
 
 class PokerEngine(object):
@@ -119,19 +189,42 @@ class PokerEngine(object):
 
     def deal_community(self):
         community_cards = self.cards[:5]
+        community_cards = map(Card, community_cards)
         community = Community(community_cards)
         return community
 
 
-def main():
-    num_players = 10
-    pg = PokerEngine()
+class Player(object):
 
+    def __init__(self):
+        pass
+
+
+def play_game(num_players):
+    pg = PokerEngine()
     hands = pg.deal_hands(num_players)
     card = Card(12)
     print(card)
     hand = hands[0]
     print(hand)
+    community = pg.deal_community()
+    print(community)
+    community.next()
+    print(community)
+
+
+def main():
+    num_players = 10
+    pg = PokerEngine()
+    hands = pg.deal_hands(num_players)
+    card = Card(12)
+    print(card)
+    hand = hands[0]
+    print(hand)
+    community = pg.deal_community()
+    print(community)
+    community.next()
+    print(community)
 
 
 if __name__ == '__main__':
