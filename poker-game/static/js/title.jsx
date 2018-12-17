a5efe80a964a080cd393c1c0e3dc5efecc295814
Simon <simonfong6@gmail.com>
2018-12-17 02:37:10 -0800

    Title component

---
+++ poker-game/static/js/title.jsx
@@ -0,0 +1,14 @@
+'use strict';
+
+class Title extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+
+    return (
+      <h1 className="center">Poker Game</h1>
+    );
+  }
+}
