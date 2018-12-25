5df1a66024cafd6771bca835d8a2c5e9d97d30bf
Simon <simonfong6@gmail.com>
2018-12-25 03:46:46 -0800

    Made test suits lowercase

--- tests/test_hands.py
+++ tests/test_hands.py
@@ -30,32 +30,32 @@ class TestHands(unittest.TestCase):
         Tests the find pairs function.
         """
         # Test no pairs.
-        hole_cards = ['3S', '8D']
-        community = ['2H', '7S', 'AS']
+        hole_cards = ['3s', '8d']
+        community = ['2h', '7s', 'As']
         pairs_corrrect = None
         pairs = hands.find_pairs(hole_cards, community)
         self.assertEqual(pairs, pairs_corrrect)
 
         # Test one pair.
-        hole_cards = ['2S', '8D']
-        community = ['2H', '7S', 'AS']
-        pairs_corrrect = [set(['2S', '2H']), set(['8D', '7S', 'AS'])]
+        hole_cards = ['2s', '8d']
+        community = ['2h', '7s', 'As']
+        pairs_corrrect = [set(['2s', '2h']), set(['8d', '7s', 'As'])]
         pairs = hands.find_pairs(hole_cards, community)
         pairs = TestHands.to_set(pairs)
         self.assertEqual(pairs, pairs_corrrect)
 
         # Test two pair.
-        hole_cards = ['2S', '7D']
-        community = ['2H', '7S', 'AS']
-        pairs_corrrect = [set(['7D', '7S']), set(['2S', '2H']), set(['AS'])]
+        hole_cards = ['2s', '7d']
+        community = ['2h', '7s', 'As']
+        pairs_corrrect = [set(['7d', '7s']), set(['2s', '2h']), set(['As'])]
         pairs = hands.find_pairs(hole_cards, community)
         pairs = TestHands.to_set(pairs)
         self.assertEqual(pairs, pairs_corrrect)
 
         # Test three pair.
-        hole_cards = ['2S', '7D']
-        community = ['2H', '7S', 'AS', '6S', 'AC']
-        pairs_corrrect = [set(['AS', 'AC']), set(['7D', '7S']), set(['6S'])]
+        hole_cards = ['2s', '7d']
+        community = ['2h', '7s', 'As', '6s', 'Ac']
+        pairs_corrrect = [set(['As', 'Ac']), set(['7d', '7s']), set(['6s'])]
         pairs = hands.find_pairs(hole_cards, community)
         pairs = TestHands.to_set(pairs)
         self.assertEqual(pairs, pairs_corrrect)
@@ -65,16 +65,16 @@ class TestHands(unittest.TestCase):
         Tests the find three-of-a-kind function.
         """
         # Test no three-of-a-kind.
-        hole_cards = ['3S', '3D']
-        community = ['2H', '7C', 'AS']
+        hole_cards = ['3s', '3d']
+        community = ['2h', '7c', 'As']
         hand_correct = None
         hand = hands.find_three_of_a_kind(hole_cards, community)
         self.assertEqual(hand, hand_correct)
 
         # Test one three-of-a-kind.
-        hole_cards = ['3S', '3D']
-        community = ['2H', '3C', 'AS']
-        hand_correct = [set(['3S', '3D', '3C']), set(['2H', 'AS'])]
+        hole_cards = ['3s', '3d']
+        community = ['2h', '3c', 'As']
+        hand_correct = [set(['3s', '3d', '3c']), set(['2h', 'As'])]
         hand = hands.find_three_of_a_kind(hole_cards, community)
         hand = TestHands.to_set(hand)
         self.assertEqual(hand, hand_correct)
